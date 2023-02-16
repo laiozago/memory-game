89c512c97378d9d7380bcbda2d21c4d8f526f8eb
laiozago <laiozago@gmail.com>
2023-02-15 23:02:55 -0300

    tabuleiro redondo

--- fluxograma.md
+++ fluxograma.md
@@ -1,9 +1,6 @@
->Criar uma array para armazenar as cores que serão exibidas no jogo.
->Criar um método para aleatoriamente embaralhar as cores da array.
->Adicionar event listeners para cada quadrado, para detectar quando eles são clicados.
->Quando um quadrado é clicado, exibir a cor correspondente e armazená-lo em uma variável temporária.
->Quando outro quadrado é clicado, verificar se sua cor corresponde à cor armazenada na variável temporária.
->Se as cores corresponderem, adicionar um ponto ao jogador. Se não corresponderem, remover a exibição das cores dos quadrados.
->Armazenar a pontuação máxima em um local storage ou em uma variável.
->Atualizar a pontuação e o recorde a cada jogada.
->Quando todas as cores corresponderem, exibir uma mensagem de fim de jogo e oferecer a opção de recomeçar o jogo.
+> adicionar evento de clique no botao começar
+> alerta com o numero sorteado
+> clicar nos quadrados ate formar um array do mesmo tamanho do array de numeros sorteados
+> comparar os arrays
+>> se certo continua pra próxima iteração
+>> se errado encerra 
--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 //* Description: Jogo da memória
+
+//seleciona o botão
+const btn = document.querySelector('#btn');
+//adiciona o evento de click no botão
+btn.addEventListener("click", jogo);
+
 function jogo() {
-    const numeros = [1,2,3,4,5,6];
+    const numeros = [1,2,3,4];
     let sequencia = [];
     let certo = true;
+    let pontuacao = sequencia.length;
+    let marcaPontuacao = document.querySelector('#marca-pontuacao');
 
     while (certo) {
         let num = Math.floor(Math.random() * numeros.length+1);
         sequencia.push(num);
         confirm(`Sequência: ${sequencia}`);
-        let resposta = prompt(`Digite a sequência:`);
+
+        let arrayResposta = [];
+        while(arrayResposta.length != sequencia.length){
+            if(quadradoClicado()){
+            arrayResposta.push(quadradoClicado());
+            }
+        }
+        let resposta = arrayResposta;
 
         if (resposta == sequencia) {
+            pontuacao++;
+            marcaPontuacao.innerHTML = `Pontuação: ${pontuacao}`;
             alert(`Parabéns! Você acertou!`);
             certo = true;
         } else {
@@ -20,7 +37,23 @@ function jogo() {
     }
 }
 
-//seleciona o botão
-const btn = document.querySelector('#btn');
-//adiciona o evento de click no botão
-btn.addEventListener("click", jogo);
+//função retorna o numero do quadrado clicado
+function quadradoClicado() {
+    let quadrado = this.id;
+    return quadrado;
+}
+
+//função que adiciona o evento de click nos quadrados
+function addEventos() {
+    const quadrados = document.querySelectorAll('.quadrado');
+    quadrados.forEach(quadrado => {
+        quadrado.addEventListener('click', quadradoClicado);
+    });
+}
+
+//função cria array com o número dos quadrados clicados
+function criaArray() {
+    let array = [];
+    array.push(quadradoClicado());
+    return array;
+}
